00ce466f1e8d2161b8b85da3b01d89697613a0ad
amirthenew <ah.abshari@hotmail.com>
2023-01-28 01:45:26 +0330

    remove button added

--- todo.js
+++ todo.js
@@ -21,7 +21,8 @@ function taskMaker (){
     list.appendChild(taskBar)
     const task = inputField.value
     tasks.push(task)
-    taskBar.innerHTML =  task+`<img class='tickbutton' style='width:10px' src="./img/done.png" alt="">`
+    taskBar.innerHTML =  task+`<img class='tickbutton' style='width:10px' src="./img/done.png" alt=""> 
+    <img class='removebutton' style='width:10px' src="./img/remove.png" alt="">`
     inputField.value =''
 
 }
@@ -30,14 +31,31 @@ function taskMaker (){
 
 list.addEventListener('click',taskDone)
 
+
+
 function taskDone (e){
 console.log(e.target.parentNode);
 
-e.target.parentNode.innerHTML
-console.log(e.target.parentNode.childNodes[0]);
-
-  e.target.parentNode.classList.toggle('doneclass')  
+if (e.target.parentNode.classList.contains(list)){
+    alert('ghad')
+    retun
 }
+
+
+
+let itemSelected = e.target.parentNode
+    itemSelected.classList.toggle('doneclass') 
+    
+    if (e.target.classList.contains('removebutton')){
+        e.target.parentNode.innerText = ''
+        console.log(e.target.parentNode);
+}
+}
+
+
+
+
+
 
 function clear (){
     list.innerHTML=''
